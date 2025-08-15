21de457072ef13f8f65acba1547a24e758a02a4f
lokerjogja-commits <lowkerjogja.cf@gmail.com>
2025-08-16 02:13:33 +0700

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,9 +1,6 @@
-const { DateTime } = require("luxon");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addFilter("date", (dateObj, format = "yyyy") => {
-    if(!dateObj) return "";
-    return DateTime.fromJSDate(new Date(dateObj), { zone: 'utc' }).toFormat(format);
+  eleventyConfig.addFilter("year", () => {
+    return new Date().getFullYear();
   });
 
   return {
