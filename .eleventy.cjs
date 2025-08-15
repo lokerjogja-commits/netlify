b6788c8f1e67c176bea0c7c663ccbf20cee4333d
lokerjogja-commits <lowkerjogja.cf@gmail.com>
2025-08-16 02:11:19 +0700

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,9 +1,16 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("date", (dateObj, format = "yyyy") => {
+    if(!dateObj) return "";
+    return DateTime.fromJSDate(new Date(dateObj), { zone: 'utc' }).toFormat(format);
+  });
+
   return {
     dir: {
-      input: "src",        // folder sumber
-      includes: "_includes", // folder includes di dalam src
-      output: "_site"      // folder hasil build
+      input: "src",
+      includes: "_includes",
+      output: "_site"
     }
   };
 };
